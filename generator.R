46904aeedeb3889dff1ef1f762100bec0d52406a
maryamayat <maryam.ayat@gmail.com>
2023-08-10 17:23:01 -0400

    Added uniform generator

--- generator.R
+++ generator.R
@@ -1,3 +1,7 @@
 # Generate a random number from Standard Normal Distribution
 x<-rnorm(1)
 print(paste("rand number (normal)=", x))
+
+# Generate a random number from Uniform Distribution
+x<-runif(1)
+print(paste("rand number (uniform)=", x))
